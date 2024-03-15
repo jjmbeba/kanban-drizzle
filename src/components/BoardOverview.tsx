5cf4ed3db164aecc008920b8ec1f4847a77df730
Johnson Mbeba <jjmbeba@gmail.com>
2024-03-15 18:25:14 +0300

    fix: Fix import error

--- src/components/BoardOverview.tsx
+++ src/components/BoardOverview.tsx
@@ -1,10 +1,6 @@
 "use client";
 
-import { getBoards } from "@/db/actions";
 import { useSidebarStore } from "@/store/sidebarStore";
-import { useUser } from "@clerk/nextjs";
-import { useQuery } from "@tanstack/react-query";
-import React from "react";
 
 const BoardOverview = () => {
   const showSidebar = useSidebarStore((state) => state.showSidebar);
